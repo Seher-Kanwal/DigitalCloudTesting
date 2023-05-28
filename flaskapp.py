0af97453b4a86b10d99f6b6e799e1158464e0010
Seher Kanwal <92606737+Seher-Kanwal@users.noreply.github.com>
2023-05-28 08:48:54 +0500

    Sentiments included

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,11 @@
 import difflib
 from flask import Flask
+import gensim.downloader as api
+import numpy as np
 import pickle
 
+clf = pickle.load(open('gradientBoosting.pkl', 'rb'))
+wv = api.load('word2vec-google-news-300')
 
 df = pickle.load(open('df.pkl', 'rb'))
 destinations = pickle.load(open('destinations.pkl', 'rb'))
@@ -12,7 +16,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World 123'
+    return 'Hello In the World of Digital Cloud'
 
 
 @app.route('/Recommendation/<string:desti_name>')
@@ -23,19 +27,59 @@ def Recommendations(desti_name):
     index_of_desti = df[df['_key'] == closed_match].index.values[0]
     similarity_score = list(enumerate(similarity[index_of_desti]))
     sorted_desti_list = sorted(similarity_score, key=lambda x: x[1], reverse=True)
-    print("Destination Suggested to you are : ")
+    # print("Destination Suggested to you are : ")
     name = []
     for desti in sorted_desti_list:
         index = desti[0]
         name.append(df.iloc[index]['_key'])
 
-    print("Top 5 similar Items:")
+    # print("Top 5 similar Items:")
     names = []
     for i in range(1, 6):
-        print(i, ": ", name[i])
+        # print(i, ": ", name[i])
         names.append(name[i])
 
     return names
+
+
+@app.route('/Scoring<string/<string:comments>')
+def Scoring(comments):
+    comments_list = comments.split('///')
+    scoring_comments = []
+    for comment in comments_list:
+        scoring_comments.append(predict(comment))
+
+    n = 0
+    for i in range(len(scoring_comments)):
+        if scoring_comments[i] >= 3:
+            n = n + 1
+
+    print(n)
+    print(scoring_comments)
+    percentage = (n / len(scoring_comments)) * 100
+    return int(percentage)
+
+
+def predict(comment):
+    token = sent_vec(comment)
+    x2d = [np.stack(token)]
+    y = clf.predict(x2d)
+    # print(y)
+    x = str(y[0])
+    return x
+
+
+def sent_vec(sent):
+    vector_size = wv.vector_size
+    wv_res = np.zeros(vector_size)
+    # print(wv_res)
+    ctr = 1
+    for w in sent:
+        if w in wv:
+            ctr += 1
+            wv_res += wv[w]
+    wv_res = wv_res / ctr
+    return wv_res
 
 
 # main driver function
